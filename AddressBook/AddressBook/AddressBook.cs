2d41e4b4a6ed93ca27b429c65111d37ce6a5a288
67418_Rutuja <rutubkakad@gmail.com>
2024-12-26 08:07:11 +0530

    Tried to solve the problem

---
+++ AddressBook/AddressBook/AddressBook.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook
+{
+    internal class AddressBook 
+    {
+            public List<Contact> contacts = new List<Contact>();
+        
+        
+
+        public void AddContacts(Contact contact)
+        {
+            contacts.Add(contact);
+            Console.WriteLine("Contacts added to the AddressBook");
+
+        }
+
+       public void EditContact(string firstName, string lastName)
+       {
+            var contact = contacts.Where( x => x.FirstName == firstName  && x.LastName == lastName).FirstOrDefault();
+            if (contact != null)
+            {
+                Console.WriteLine("Enter new details:");
+                Console.Write("Address: ");
+                contact.Address = Console.ReadLine();
+                Console.Write("City: ");
+                contact.City = Console.ReadLine();
+                Console.Write("State: ");
+                contact.State = Console.ReadLine();
+                Console.Write("Zip: ");
+                contact.Zip = Console.ReadLine();
+                Console.Write("Phone Number: ");
+                contact.PhoneNo = Console.ReadLine();
+                Console.Write("Email: ");
+                contact.Email = Console.ReadLine();
+                Console.WriteLine("Contact updated successfully.");
+            }
+            else
+            {
+                Console.WriteLine("Contact is not valid");
+            }
+       }
+        public void DeleteContact(string firstName, string lastName)
+        {
+            var contact = contacts.FirstOrDefault(y => y.FirstName == firstName && y.LastName == lastName);
+            if (contact != null)
+            {
+                contacts.Remove(contact);
+                Console.WriteLine("Contact deleted successfully");
+            }
+        }
+
+        public void DisplayContact()
+        {
+            if (contacts.Count == 0)
+            {
+                Console.WriteLine("AddressBook is empty");
+            }
+            else
+            {
+                foreach (var contact in contacts)
+                {
+                    Console.WriteLine(contact);
+                }
+            }
+        }
+       
+            
+            
+
+
+    }
+}
---
+++ AddressBook/AddressBook/Program.cs
@@ -0,0 +1,68 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+using System;
+namespace AddressBook
+{
+    class AddressBookMain
+    {
+        static void Main(string[] args)
+        {
+
+            AddressBook address = new AddressBook();
+            Contact newContact = new Contact();
+
+            Console.WriteLine("\nAddress Book Menu:");
+            Console.WriteLine("1. Add Contact");
+            Console.WriteLine("2. Edit Contact");
+            Console.WriteLine("3. Delete Contact");
+            Console.WriteLine("4. Display Contacts");
+            Console.WriteLine("5. Exit");
+            Console.Write("Enter your choice: ");
+
+            int choice = int.Parse(Console.ReadLine());
+            switch (choice)
+            {
+                case 0:
+                    Console.WriteLine("First Name");
+                    newContact.FirstName = Console.ReadLine();
+                    Console.WriteLine("Last Name");
+                    newContact.LastName = Console.ReadLine();
+                    Console.WriteLine("Address");
+                    newContact.Address = Console.ReadLine();
+                    Console.WriteLine("City");
+                    newContact.City = Console.ReadLine();
+                    Console.WriteLine("State");
+                    newContact.State = Console.ReadLine();
+                    Console.WriteLine("Zip");
+                    newContact.Zip = Console.ReadLine();
+                    Console.WriteLine("PhoneNo");
+                    newContact.PhoneNo = Console.ReadLine();
+                    Console.WriteLine("Email");
+                    newContact.Email = Console.ReadLine();
+
+                    address.AddContacts(newContact);
+                    break;
+                case 1:
+                    Console.Write("Enter the Firsname of contact to edit: ");
+                    string editFirstName = Console.ReadLine();
+                    Console.Write("Enter the Lastname of contact to edit: ");
+                    string editLastName = Console.ReadLine();
+                    address.EditContact(editFirstName, editLastName);
+                    break;
+                case 2:
+                    Console.Write("Enter the Firsname of contact to delete: ");
+                    string deleteFirstName = Console.ReadLine();
+                    Console.Write("Enter the Lastname of contact to delete: ");
+                    string deleteLastName = Console.ReadLine();
+                    address.EditContact(deleteFirstName, deleteLastName);
+                    break;
+                case 3:
+                    address.DisplayContact();
+                    break;
+
+
+            }
+        }
+            
+    }
+}
